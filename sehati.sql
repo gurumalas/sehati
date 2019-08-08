d670a3b92b862f0d560005b600a4839bb9dfe7c2
RI2N <akhmadriduan66@gmail.com>
2019-08-08 07:39:47 +0330

    sqlnya update

--- sehati.sql
+++ sehati.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2019 at 06:19 AM
+-- Generation Time: Aug 08, 2019 at 06:09 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -66,14 +66,28 @@ CREATE TABLE `invoice` (
 --
 
 INSERT INTO `invoice` (`kd_transaksi`, `kd_invoice`, `nik`, `tgl`, `kd_produk`, `totalbyar`, `status`, `quantity`, `harga`) VALUES
-('T00001', 1, 0, '2019-08-05', '5467', '195', '0', '3', '65'),
-('T00001', 2, 0, '2019-08-05', '6666666666', '30', '0', '3', '10'),
-('T00001', 3, 1, '2019-08-05', '5467', '195', '0', '3', '65'),
-('T00001', 4, 1, '2019-08-05', '6666666666', '30', '0', '3', '10'),
+('T00001', 1, 0, '2019-07-05', '5467', '195', '1', '3', '65'),
+('T00001', 2, 0, '2019-07-05', '6666666666', '30', '0', '3', '10'),
+('T00001', 3, 1, '2019-07-05', '5467', '195', '0', '3', '65'),
+('T00001', 4, 1, '2019-07-05', '6666666666', '30', '1', '3', '10'),
 ('T00002', 5, 1, '2019-08-05', '5467', '195', '0', '3', '65'),
 ('T00002', 6, 1, '2019-08-05', '6666666666', '30', '0', '3', '10'),
 ('T00003', 7, 1, '2019-08-05', '5467', '195', '0', '3', '65'),
-('T00003', 8, 1, '2019-08-05', '6666666666', '30', '0', '3', '10');
+('T00003', 8, 1, '2019-08-05', '6666666666', '30', '0', '3', '10'),
+('', 9, 0, '2019-08-05', '5467', '1', '', '1', NULL),
+('', 10, 0, '2019-08-05', '6666666666', '1', '', '1', NULL),
+('T00004', 11, 1, '2019-08-05', '5467', '130', '0', '2', '65'),
+('T00004', 12, 1, '2019-08-05', '6666666666', '30', '1', '3', '10'),
+('', 13, 0, '2019-08-05', '5467', '1', '', '1', NULL),
+('T00005', 14, 123, '2019-08-05', '5467', '260', '0', '4', '65'),
+('T00006', 15, 123, '2019-08-05', '5467', '260', '1', '4', '65'),
+('', 16, 0, '2019-08-05', '5467', '1', '', '1', NULL),
+('', 17, 0, '2019-08-07', '5467', '1', '', '1', NULL),
+('', 18, 0, '2019-08-07', '5467', '2', '', '2', NULL),
+('', 19, 0, '2019-08-07', '9', '1', '', '1', NULL),
+('', 20, 0, '2019-08-07', '5467', '1', '', '1', NULL),
+('', 21, 0, '2019-08-07', '9', '1', '', '1', NULL),
+('', 22, 0, '2019-08-07', '5467', '2', '', '2', NULL);
 
 -- --------------------------------------------------------
 
@@ -120,11 +134,28 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`nik`, `n_member`, `jk`, `hp`, `email`, `kota`, `kodepos`, `alamat`, `fotomember`, `password`) VALUES
-('1', 'duan', 1, '', 'akhmadriduan.umm.ac.id', '', '', '', 'giphy.gif', 'c060ecbf17fdef81cf4c25b26fdc97da'),
-('2', '', 1, '', '', '', '', '', 'si cantik.jpg', ''),
-('3', '', 1, '', '', '', '', '', 'si cantik.jpg', ''),
+('1', 'riduan', 1, '77778', 'akhmadriduan.umm.ac.id', 'BJB', '897', '', 'giphy.gif', '202cb962ac59075b964b07152d234b70'),
+('123', 'muhammad iqbal', 1, '082148352769', 'rendy.septian98@yahoo.com', 'banjarbaru', '70724', 'aaaa', '8138193836_64d71b9205.jpg', '60496a1f3d8615e89c38575c39a0aaa7'),
 ('7', '', 1, '', '', '', '', '', 'AADKfPE.jpg', ''),
-('9', '', 1, '', '', '', '', '', 'merawat kuku.png', '21232f297a57a5a743894a0e4a801fc3');
+('9', 'Nanda', 0, '', '', '', '', '', 'merawat kuku.png', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owner`
+--
+
+CREATE TABLE `owner` (
+  `no` int(11) NOT NULL,
+  `nama` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `owner`
+--
+
+INSERT INTO `owner` (`no`, `nama`) VALUES
+(1, 'H. Ramlan');
 
 -- --------------------------------------------------------
 
@@ -148,7 +179,7 @@ CREATE TABLE `produk` (
 
 INSERT INTO `produk` (`kd_produk`, `nama`, `foto_file`, `kd_jenis`, `harga`, `deskripsi`, `quantity`) VALUES
 ('5467', 'cicak', 'botol.gif', '1', '65', '2222', 30),
-('6666666666', 'aku', 'ulat gigi.jpg', '1', '10', 'Apapun caranya siap dilakukan ,mulai dari menjaga pola makan hingga mengatur aktifitas sehari-hari. \r\nNamun, banyak orang yang ingin menurunkan berat badan dengan cepat dan ingin cepat turun berat badan dalam waktu yang relatif singkat', 49);
+('9', 'aku', 'ulat gigi.jpg', '1', '10', '1 caranya siap dilakukan ,mulai dari menjaga pola makan hingga mengatur aktifitas sehari-hari. \r\nNamun, banyak orang yang ingin menurunkan berat badan dengan cepat dan ingin cepat turun berat badan dalam waktu yang relatif singkat', 49);
 
 --
 -- Indexes for dumped tables
@@ -179,6 +210,12 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`nik`);
 
 --
+-- Indexes for table `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`no`);
+
+--
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
@@ -192,7 +229,7 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `kd_invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `kd_invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
