dac7faddecee9ab40a7fed5248448bddb86b40f0
RI2N <akhmadriduan66@gmail.com>
2019-08-13 07:20:04 +0330

    sc

---
+++ laporan/pdfcart.php
@@ -0,0 +1,18 @@
+<?php
+ob_start();
+
+include('sc_cart.php');
+$content = ob_get_clean();
+
+require_once('../html2pdf/html2pdf.class.php');
+try {
+    $html2pdf = new HTML2PDF('P','F4','en', false, 'ISO-8859-15',array(15, 15, 15, 0));
+    // $html2pdf = new HTML2PDF('L','FOLIO','en', false, 'ISO-8859-15',array(5, 10, 10, 0));
+    $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+    ob_end_clean();
+    $html2pdf->Output('Keranjang.pdf');
+}
+catch(HTML2PDF_exception $e) { echo $e; }
+
+
+?>
